0ab191b3f557cf3ae33e8a4d8dd73eeca6871aa2
AishwaryaNair687 <59355271+AishwaryaNair687@users.noreply.github.com>
2020-09-17 20:46:18 +0530

    signup file has been added

--- Headers/SignUp.h
+++ Headers/SignUp.h
@@ -1,7 +1,45 @@
 #include <iostream>
-
+#include <string.h>
+#include"users.h"
 using namespace std;
-
+void users::sign_up()
+{ cout<<"PLEASE MENTION OUR DETAILS BELOW \n";
+  cout << "Please Enter usersname:\n";
+  cin >> users_name;
+  cout<<"Please enter your course name:\n";
+  cin>>course;
+  cout<<"Please enter your current year:\n";
+  cin>>year;
+  cout<<"Please enter your ROLL NO.:\n";
+  cin>>rollno;
+  start:
+  cout << "Please Enter Password(password must be atleast 8 characters):\n ";
+  cin >> password;
+  if (password.length()< 8)
+  {
+    cout << "Password must be having atleast 8 characters\n";
+	getchar();
+	goto start;
+  }
+  else
+  { int i=0;
+	  back:
+    cout << "Please Re-Enter the Password again:\n ";
+    cin >> passwordcheck;
+    if (password.compare(passwordcheck)==0){
+      cout << "You are registered successfully!!!!!:)\n";
+    }
+	else{
+      cout << "The password previously entered is not matching with the current password :(\n";
+	  i++;
+     if(i<3)
+	  goto back;
+	  else{
+		  cout<<"SEEMS LIKE YOU ENTERED MISMATCHING PASSWORD MANY TIMES PLEASE REGISTER AGAIN :(\n";
+	  }
+  }
+}}
 void signup(){
-    cout<<"signup.....";
+    users obj1;
+    obj1.sign_up();
 }
