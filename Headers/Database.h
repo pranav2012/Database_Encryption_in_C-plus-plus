636c932706a47b63702713410e028453223ef99b
sonakshi2500@gmail.com <global user.email"sonakshi2500@gmail.com>
2020-09-20 15:55:41 +0530

    .

--- Headers/Database.h
+++ Headers/Database.h
@@ -1,7 +1,52 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sqlite3.h> 
 
-using namespace std;
+static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
+   int i;
+   for(i = 0; i<argc; i++) {
+      printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+   }
+   printf("\n");
+   return 0;
+}
 
-void db(){
-    cout<<"database....";
+int main(int argc, char* argv[]) {
+   sqlite3 *db;
+   char *zErrMsg = 0;
+   int rc;
+   char *sql;
+   char username[50], password[50];
+   
+   printf("Enter username\n");
+   gets(username);
+   printf("Enter password\n");
+   gets(password);
+   
+   /* Open database */
+   rc = sqlite3_open("signupdb.sqlite", &db);
+   
+   if( rc ) {
+      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+      return(0);
+   } else {
+      fprintf(stderr, "Opened database successfully\n");
+   }
+
+   /* Create SQL statement */
+   sql = "INSERT INTO USER_INFO (USERNAME,PASSWORD) "  \
+         "VALUES (username,password); " ;
+
+   /* Execute SQL statement */
+   rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+   
+   if( rc != SQLITE_OK ){
+      fprintf(stderr, "SQL error: %s\n", zErrMsg);
+      sqlite3_free(zErrMsg);
+   } else {
+      fprintf(stdout, "Records created successfully\n");
+   }
+   sqlite3_close(db);
+   return 0;
 }
