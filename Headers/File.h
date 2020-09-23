843b6edb2dd6971d753cfc3fd57ec9f4dd9aa16a
Lakshika <lavii.bansall@gmail.com>
2020-09-24 01:13:43 +0530

    lakshika

--- Headers/File.h
+++ Headers/File.h
@@ -1,10 +1,41 @@
-#ifndef FILE_H
-#define FILE_H
 #include <iostream>
-
+#include <fstream>
+#include <string.h>
 using namespace std;
+int main()
+{
+  
+  string name;
+  int prn;
+  string course;
+  
+  fstream file;
+  file.open ("example.txt", ios::out | ios::in );
 
-void tofile(){
-    cout<<"file hadling.....";
+  cout << "\n Enter your name:" ;
+  getline(cin, name);
+  
+  file << name <<endl;
+  file >> name;
+  cout << name << endl;
+  
+  cout << "\n Enter your roll number:" ;
+  cin >> prn ;
+  
+  file << prn <<endl;
+  file >> prn;
+  cout << prn << endl;
+  
+  cout << "\n Enter your course:" ;
+  getline(cin, course);
+  getline(cin, course);
+  
+  file << course <<endl;
+  file >> course;
+  cout << course << endl;
+
+  file.close();
+  
+  return 0;
 }
-#endif
+
